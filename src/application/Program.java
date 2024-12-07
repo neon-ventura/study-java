c3869d1ca348e37712aca61194e7a47525200630
neon-notebook <magda.neon@gmail.com>
2024-12-06 23:22:37 -0300

    new commit for neon-notbook realy

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,7 +4,7 @@ package application;
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("new commit in neon-notebook!");
+		System.out.println("new commit in neon-notebook now really!");
 	}
 
 }
