ab0bf6e44649eecb5ffd781ea021f32266f101ca
neon-notebook <magda.neon@gmail.com>
2024-12-11 10:13:48 -0300

    new project finished

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,29 @@
 package application;
 
+import java.util.Scanner;
+
+import model.entities.PrintService;
+
 
 public class Program {
 
 	public static void main(String[] args) {
+		PrintService print = new PrintService();
+		
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("How many values: ");
+		
+		int quantity = in.nextInt();
+		
+		for(int i = 0; i < quantity; i++) {
+			System.out.print("Value [" + i + "]: ");
+			int value = in.nextInt();
+			print.addValue(value);
+		}
+		
+		print.printList();
+		
 		
 	}
 
