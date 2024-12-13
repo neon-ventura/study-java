448e08e002992c8630e38bebc3db2e0519cceea7
Neon Ventura <123643824+neon-ventura@users.noreply.github.com>
2024-12-12 22:31:57 -0300

    HashCodes e Equals

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,30 +1,13 @@
 package application;
 
-import java.util.Scanner;
-
-import model.entities.PrintService;
-
-
 public class Program {
 
 	public static void main(String[] args) {
-		PrintService<Integer> print = new PrintService<>();
+		String a = "Neon";
+		String b = "Alex";
 		
-		Scanner in = new Scanner(System.in);
-		
-		System.out.println("How many values: ");
-		
-		int quantity = in.nextInt();
-		
-		for(int i = 0; i < quantity; i++) {
-			System.out.print("Value [" + i + "]: ");
-			int value = in.nextInt();
-			print.addValue(value);
-		}
-		
-		print.printList();
-		
-		in.close();
+		System.out.println(a.hashCode());
+		System.out.println(b.hashCode());
 	}
 
 }
